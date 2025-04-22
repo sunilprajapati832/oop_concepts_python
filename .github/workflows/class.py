aff61a55b70511bf5651a970a7019f4ca05cdd74
Sunil Prajapati <sunilp832@gmail.com>
2025-04-22 10:12:41 +0530

    Update class.py

--- .github/workflows/class.py
+++ .github/workflows/class.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        print("Starting engine...")
+    
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, num_doors):
+        super().__init__(make, model, year)
+        self.num_doors = num_doors
+
+    def drive(self):
+        print("Driving the car...")
+
+vehicle = Vehicle("Toyota", "Corolla", 2022)
+car = Car("Tesla", "Model", 2021, 4)
+
+vehicle.start_engine
+car.start_engine
+
+car.drive
+
